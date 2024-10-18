f4444aa19f1cf7da999f4c4a286ce7d21a96d6db
Hubery7777 <xiao-main@foxmail.com>
2024-10-18 21:17:46 +0800

    clippy_conversions_tests_5to9

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,7 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
+
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
@@ -36,14 +36,17 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    pub fn my_demo_function(a: u32) -> u32 {
         a
+    }
+    pub fn my_demo_function_alias(a: u32) -> u32 {
+        my_demo_function(a)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::Foo::*;
 
     #[test]
     fn test_success() {
@@ -53,9 +56,7 @@ mod tests {
         //
         // SAFETY: We know those functions are aliases of a safe
         // Rust function.
-        unsafe {
-            my_demo_function(123);
-            my_demo_function_alias(456);
-        }
+        assert_eq!(my_demo_function(123), 123);
+        assert_eq!(my_demo_function_alias(456), 456);
     }
 }
